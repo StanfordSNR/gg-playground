e9e6ff8ba87578b894a5459945ccb4c5cdefc1a2
Dan Iter <daniter@stanford.edu>
2017-05-05 13:56:24 -0700

    added utils for capturing and parsing traces

---
+++ targets/make-4.2/trace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+unset PATH
+export HOME=/home/daniter
+export PATH=/home/daniter/workspace/gg-playground/targets/toolchain/x86_64-linux-musl/bin
+/usr/bin/strace -f -s 120 -e trace=file -o /tmp/musl-trace.out x86_64-linux-musl-gcc -g -O2 -frandom-seed=daniter -c -o remake.o remake.i
+/usr/bin/strace -f -s 120 -e trace=file -o /tmp/musl-trace-cc.out x86_64-linux-musl-gcc -g -O2 -frandom-seed=daniter -S -c -o remake.s remake.i
+/usr/bin/strace -f -s 120 -e trace=file -o /tmp/musl-trace-as.out x86_64-linux-musl-gcc -g -O2 -frandom-seed=daniter -c -o remake.o remake.s
+exit()
