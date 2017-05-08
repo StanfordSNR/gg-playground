6bd3d520877a3ba30e5a8e461de737b8d3c6480b
Dan Iter <daniter@stanford.edu>
2017-05-08 13:10:05 -0700

    fixing debug script

--- targets/make-4.2/debug.sh
+++ targets/make-4.2/debug.sh
@@ -3,5 +3,5 @@
 unset PATH
 export HOME=/home/daniter
 export PATH=/home/daniter/workspace/gg-playground/targets/toolchain/x86_64-linux-musl/bin
-/usr/bin/gdb --args x86_64-linux-musl-gcc -g -O2 -frandom-seed=daniter -c -o remake.o remake.i
-exit()
+/usr/bin/gdb --args x86_64-linux-musl-gcc -g -O2 -frandom-seed=daniter -c -o remake.o remake.s
+exit 0
