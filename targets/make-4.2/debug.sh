edad18a979216e47d36cd24defeef46e7531cc6e
Dan Iter <daniter@stanford.edu>
2017-05-11 18:41:40 -0700

    removed extra binutils. added env vars to trace

--- targets/make-4.2/debug.sh
+++ targets/make-4.2/debug.sh
@@ -3,5 +3,6 @@
 unset PATH
 export HOME=/home/daniter
 export PATH=/home/daniter/workspace/gg-playground/targets/toolchain/x86_64-linux-musl/bin
+export GG=1
 /usr/bin/gdb --args x86_64-linux-musl-gcc -S -g -O2 -frandom-seed=daniter -c -o remake.s remake.i
 exit 0
--- targets/make-4.2/trace.sh
+++ targets/make-4.2/trace.sh
@@ -2,7 +2,12 @@
 
 # Run with env -i to run without environment variables
 unset PATH
-#export remake_i=__GG_REPLACE__
+export _home_daniter_workspace_gg_playground_targets_toolchain_x86_64_linux_musl_lib_gcc_x86_64_linux_musl_6_3_0_cc1='/home/daniter/workspace/gg-playground/targets/toolchain/x86_64-linux-musl/lib/gcc/x86_64-linux-musl/6.3.0/cc1'
+#export ___lib_gcc_x86_64_linux_musl_6_3_0_='../lib/gcc/x86_64-linux-musl/6.3.0/'
+export remake_i='remake.i'
+export _home_daniter_workspace_gg_playground_targets_toolchain_x86_64_linux_musl_bin_x86_64_linux_musl_gcc='/home/daniter/workspace/gg-playground/targets/toolchain/x86_64-linux-musl/bin/x86_64-linux-musl-gcc'
+export _tmp="/tmp"
+export GG=1
 export HOME=/home/daniter
 export PATH=/home/daniter/workspace/gg-playground/targets/toolchain/x86_64-linux-musl/bin
 /usr/bin/strace -f -s 120 -e trace=file -o /tmp/musl-trace.out x86_64-linux-musl-gcc -g -O2 -frandom-seed=daniter -c -o remake.o remake.i
